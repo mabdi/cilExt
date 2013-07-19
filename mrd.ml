59b4aa4753f473de1cfa2b34ca9843338d405470
mabdi <m.abdi@aut.ac.ir>
2013-07-19 17:23:39 +0430

    Update mrd.ml

--- mrd.ml
+++ mrd.ml
@@ -15,15 +15,16 @@
  *      |               |                   |
  *    i. Statement Id is CIL s.sid
  *    ii. Statement Type:
- * 	1 for Instruction set
- *	2 for If
- *	3 for Goto
- *	4 for Break
- *	5 for Continue
- *	6 for Return
- *	7 for Block
- *	8 for Switch
- *	9 for Loop
+ * 	SET
+ *	CALL
+ *	If
+ *	Goto
+ *	Break
+ *	Continue
+ *	Return
+ *	Block
+ *	Switch
+ *	Loop
  *
  *
  * Expression type Table:
@@ -36,42 +37,94 @@
  *
  *    i. Foreign Key to Previous Table
  *    ii. Expression Type:
- *	100 Unary operator - Neg (-)
- *	101 Unary operator - BNot (~) 
- *	102 Unary operator - LNot (!)
- *	200 Binary operator - PlusA  (*	arithmetic +    *)
- *	201 Binary operator - PlusPI (*	pointer + integer	*)
- *	202 Binary operator - IndexPI (*	pointer + integer       *)
- *	203 Binary operator - MinusA	(*	arithmetic -	*)
- *	204 Binary operator - MinusPI	(*	pointer - integer	*)
- *	205 Binary operator - MinusPP	(*	pointer - pointer	*)
- *	206 Binary operator - Mult
- *	207 Binary operator - Div	(*	/	*)
- *	208 Binary operator - Mod	(*	%	*)
- *	209 Binary operator - Shiftlt	(*	shift left	*)
- *	210 Binary operator - Shiftrt	(*	shift right	*)
- *	211 Binary operator - Lt	(*	< (arithmetic comparison)	*)
- *	212 Binary operator - Gt	(*	> (arithmetic comparison)	*)
- *	213 Binary operator - Le	(*	<= (arithmetic comparison)	*)
- *	214 Binary operator - Ge	(*	> (arithmetic comparison)	*)
- *	215 Binary operator - Eq	(*	== (arithmetic comparison)	*)
- *	216 Binary operator - Ne	(*	!= (arithmetic comparison)	*)
- *	217 Binary operator - BAnd	(*	bitwise and	*)
- *	218 Binary operator - BXor	(*	exclusive-or	*)
- *	219 Binary operator - BOr	(*	inclusive-or	*)
- *	220 Binary operator - LAnd	(*	logical and. *)
- *	221 Binary operator - LOr
- *	300 SizeOf
- *	400 CastE
- *	500 AddrOf
- *	600 StartOf
+ *	Neg (-)
+ *	BNot (~) 
+ *	LNot (!)
+ *	PlusA  	arithmetic +    
+ *	PlusPI 	pointer + integer	
+ *	IndexPI 	pointer + integer       
+ *	MinusA	arithmetic -	
+ *	MinusPI	pointer - integer	
+ *	MinusPP	pointer - pointer	
+ *	Mult
+ *	Div	/	
+ *	Mod	%	
+ *	Shiftlt	shift left	
+ *	Shiftrt	shift right	
+ *	Lt	< (arithmetic comparison)	
+ *	Gt	> (arithmetic comparison)	
+ *	Le	<= (arithmetic comparison)	
+ *	Ge	> (arithmetic comparison)	
+ *	Eq	== (arithmetic comparison)	
+ *	Ne	!= (arithmetic comparison)	
+ *	BAnd	bitwise and	
+ *	BXor	exclusive-or	
+ *	BOr	inclusive-or	
+ *	LAnd	logical and. 
+ *	LOr
+ *	SizeOf
+ *	CastE
+ *	AddrOf
+ *	StartOf
  *)
 open Cil
 open Pretty
+open Cfg
+open CrestInstrument
 module E=Errormsg
+(* CONSTANTS *)
+let valSet = 11
+let valCall = 12
+let valIf = 20
+let valGoto = 30
+let valBreak = 40
+let valContinue = 50
+let valReturn = 60
+let valBlock = 70
+let valSwitch = 80
+let valLoop = 90
 
+let valNeg = 100
+let valBNot = 101
+let valLNot = 102
+let valPlusA = 200
+let valPlusPI = 201    
+let valIndexPI = 202
+let valMinusA  = 203
+let valMinusPI = 204    
+let valMinusPP = 205
+let valMult = 206
+let valDiv = 207  
+let valMod = 208 
+let valShiftlt = 209   
+let valShiftrt = 210
+let valLt = 211
+let valGt = 212
+let valLe = 213
+let valGe = 214
+let valEq = 215  
+let valNe =216
+let valBAnd =217
+let valBXor =218
+let valBOr =219
+let valLAnd =220
+let valLOr =221
+let valSizeOf =300
+let valCastE =400
+let valAddrOf =500
+let valStartOf =600
+
+(* END OF CONSTANTS *)
 let buffert = ref ""
 let buffere = ref ""
+let fcg = ref ""
+let cfg = ref ""
+
+let wfcg f1 f2 =
+        fcg := (!fcg ^ Printf.sprintf "%d %d\n" f1 f2)
+
+let wcfg i1 i2 =
+        cfg := (!cfg ^ Printf.sprintf "%d %d\n" i1 i2)
 
 let wt i t =
 	buffert := (!buffert ^ Printf.sprintf "%d %d\n" i t)
@@ -81,110 +134,140 @@ let we i e =
 
 let rec mrdvisitExp id (e : exp) =  
    match e with
-    SizeOf -> we id 300
-  | SizeOfE e -> mrdvisitExp id e;we id 300
-  | SizeOfStr -> we id 300
-  | UnOp (u,e,_)   -> mrdvisitExp id e;(match u with Neg -> we id 100 | BNot -> we id 101 | LNot -> we id 102)
-  | BinOp (b,e1,e2,_) -> mrdvisitExp e1;mrdvisitExp e2;
+    SizeOf _ -> we id valSizeOf
+  | SizeOfE ex -> mrdvisitExp id ex; we id valSizeOf
+  | SizeOfStr _ -> we id valSizeOf
+  | UnOp (u,e,_)   -> mrdvisitExp id e;(match u with Neg -> we id valNeg | BNot -> we id valBNot | LNot -> we id valLNot)
+  | BinOp (b,e1,e2,_) -> mrdvisitExp id e1;mrdvisitExp id e2;
 	(match b with
-	 	PlusA -> we id 200
-	|	PlusPI -> we id 201
-	|	IndexPI -> we id 202	
-	|	MinusA -> we id 203	
-	|	MinusPI -> we id 204	
-	|	MinusPP -> we id 205	
-	|	Mult -> we id 206
-	|	Div -> we id 207	
-	|	Mod -> we id 208	
-	|	Shiftlt -> we id 209
-	|	Shiftrt -> we id 210	
-	|	Lt -> we id 211	
-	|	Gt -> we id 212	
-	|	Le -> we id 213	
-	|	Ge -> we id 214	
-	|	Eq -> we id 215	
-	|	Ne -> we id 216	
-	|	BAnd -> we id 217
-	|	BXor -> we id 218	
-	|	BOr -> we id 219	
-	|	LAnd -> we id 220	
-	|	LOr -> we id 221	 
+	 	PlusA -> we id valPlusA
+	|	PlusPI -> we id valPlusPI
+	|	IndexPI -> we id valIndexPI
+	|	MinusA -> we id valMinusA
+	|	MinusPI -> we id valMinusPI
+	|	MinusPP -> we id valMinusPP
+	|	Mult -> we id valMult
+	|	Div -> we id valDiv	
+	|	Mod -> we id valMod	
+	|	Shiftlt -> we id valShiftlt
+	|	Shiftrt -> we id valShiftrt
+	|	Lt -> we id valLt
+	|	Gt -> we id valGt
+	|	Le -> we id valLe
+	|	Ge -> we id valGe
+	|	Eq -> we id valEq
+	|	Ne -> we id valNe
+	|	BAnd -> we id valBAnd
+	|	BXor -> we id valBXor	
+	|	BOr -> we id valBOr	
+	|	LAnd -> we id valLAnd	
+	|	LOr -> we id valLOr	 
 )
-  | CastE (_,e)  -> we s.sid 400;mrdvisitExp e
-  | AddrOf  -> we s.sid 500
-  | StartOf -> we s.sid 600
-  | AlignOf  | AlignOfE  | Const  | Lval -> ()
+  | CastE (_,ex)  -> we id valCastE;mrdvisitExp id ex
+  | AddrOf _ -> we id valAddrOf
+  | StartOf _ -> we id valStartOf
+  | AlignOf _ | AlignOfE _ | Const _  | Lval _ -> ()
 
 and mrdvisitInstr id (i : instr) = 
    match i with
     Call(_, e, el,l) -> 
 	mrdvisitExp id e; 
-	List.iter (mrdvisitExp id) el
+	List.iter (mrdvisitExp id) el;
+	wt id valCall
   | Set (_,e,l) -> 
 	mrdvisitExp id e; 
-  | Asm -> E.s (E.unimp "Asm")
+	wt id valSet
+  | Asm _ -> E.s (E.unimp "Asm")
 
-and mrdvisitStmt (s : stmt) = 
+and mrdvisitStmt (s:stmt) = 
    match s.skind with
     Instr il  -> 
 	List.iter (mrdvisitInstr s.sid ) il;
-	wt s.sid 1
   | Return (Some e, l) -> 
-	mrdvisitExp id e;
-	wt s.sid 6
+	mrdvisitExp s.sid e;
+	wt s.sid valReturn
   | Return (None, l)  -> 
-	wt s.sid 6
-  | Goto (p,l) -> !p
-	wt s.sid 3
-  | Break l -> ()
-	wt s.sid 4
-  | Continue l -> ()
-	wt s.sid 5
+	wt s.sid valReturn
+  | Goto (p,l) ->
+	wt s.sid valGoto
+  | Break l ->
+	wt s.sid valBreak
+  | Continue l ->
+	wt s.sid valContinue
   | If (e, blk1, blk2, l) ->
-	mrdvisitExp id e;	
-	wt s.sid 2
+	mrdvisitExp s.sid e;	
+	wt s.sid valIf
   | Block b ->
-	wt s.sid 7 
+	wt s.sid valBlock
   | Switch(e,blk,sl,l) -> 
-	wt s.sid 8
-  | Loop(blk,l,Some e,None) ->
-	wt s.sid 9
-  | Loop(blk,l,None,Some e) ->
-	wt s.sid 9
-  | Loop(blk,l,None,None) ->
-	wt s.sid 9
+	wt s.sid valSwitch
+  | Loop _ ->
+	wt s.sid valLoop
   | TryExcept _ | TryFinally _ -> 
       E.s (E.unimp "try/except/finally")
 
 
 (* initialize variables *)
 let initFuncProcess (fd : fundec) =
+	buffert := ("");
+	buffere := ("");
+	cfg := ""
 
+let save filename content =
+                let f = open_out filename in
+                Printf.fprintf f "%s" content ;
+                close_out f
 
 (* write result to file *)
 let finFuncProcess (fd : fundec) = 
+	try
+		save ("mrd_st_" ^ fd.svar.vname ) !buffert;
+		save ("mrd_et_" ^ fd.svar.vname ) !buffere;
+		save ("mrd_cfg_" ^ fd.svar.vname ) !cfg
+        with x ->
+                failwith ("Failed to write in: " ^ fd.svar.vname ^ "\n")
 
-let main f:file =
+
+let rec forallStmts (todo) (fd : fundec) = 
+  begin
+    fasBlock todo fd.sbody;
+  end
+
+and fasBlock (todo) (b : block) =
+  List.iter (fasStmt todo) b.bstmts
+
+and fasStmt (todo) (s : stmt) =
+  begin
+    ignore(todo s);
+    match s.skind with
+      | Block b -> fasBlock todo b
+      | If (_, tb, fb, _) -> (fasBlock todo tb; fasBlock todo fb)
+      | Switch (_, b, _, _) -> fasBlock todo b
+      | Loop (b, _, _, _) -> fasBlock todo b
+      | (Return _ | Break _ | Continue _ | Goto _ | Instr _) -> ()
+      | TryExcept _ | TryFinally _ -> E.s (E.unimp "try/except/finally")
+  end
+
+let mrdmain (f : file) =
           Simplemem.feature.fd_doit f ;
           iterGlobals f prepareGlobalForCFG ;
           Oneret.feature.fd_doit f ;
           (let ncVisitor = new CrestInstrument.normalizeConditionalsVisitor in
              visitCilFileSameGlobals (ncVisitor :> cilVisitor) f) ;
-          Cfg.clearFileCFG f ;
+          clearFileCFG f ;
           readIdCount () ;
           readStmtCount () ;
           readFunCount () ;
-          Cfg.computeFileCFG f ;
-          handleCallEdgesAndWriteCfg f ;
-	  iterGlobals f (fun g ->
-    		match g with GFun(fd,_) -> begin
-				initFuncProcess fd;
-				Cfg.forallStmts mrdvisitInst fd;
-				finFuncProcess fd;
-			end
-    		| _ -> ()
-	  )
-
+          computeFileCFG f ;
+          handleCallEdgesAndWriteCfg f; 
+	  fcg := "";
+	  iterGlobals f (fun g -> match g with GFun (fd,_) -> 
+					initFuncProcess fd;
+					forallStmts mrdvisitStmt fd;
+					finFuncProcess fd;
+					()
+				| _ -> () );
+	  save "fcg" !fcg
 
 let feature : featureDescr =
   { fd_name = "mrd";
@@ -192,5 +275,5 @@ let feature : featureDescr =
     fd_description = "C File static Analysis for MS Thesis";
     fd_extraopt = [];
     fd_post_check = true;
-    fd_doit = main
+    fd_doit = mrdmain;
   }
